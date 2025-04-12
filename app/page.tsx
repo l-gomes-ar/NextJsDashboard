b975a7f71293d348dc36e2db3095a86306966185
l-gomes-ar <lgaraujo021@gmail.com>
2025-04-12 12:21:50 +0100

    Chapter 16 finished

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
-import Image from "next/image"
+import Image from "next/image";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
